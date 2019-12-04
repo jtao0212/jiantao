8a64c5b6b9c19360412fd9646ca182f3e999340c
jiantao <jtao0212@163.com>
2019-12-04 17:05:27 +0800

    init

---
+++ src/main/java/com/cmb/jiantao/JiantaoApplication.java
@@ -0,0 +1,13 @@
+package com.cmb.jiantao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiantaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiantaoApplication.class, args);
+    }
+
+}
